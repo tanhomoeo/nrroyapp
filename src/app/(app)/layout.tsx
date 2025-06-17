f0befb8ff9df04df78f657cc32992d02085481d3
Criminal Investigation Department (CID) <cybercrime.bd.gov@gmail.com>
2025-06-17 15:27:50 +0000

    Based on the file structure in your src/app directory, here are some pot

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/shared/AppSidebar';
-// FloatingVoiceInput import removed
+import { AppHeader } from '@/components/shared/AppHeader'; // Import AppHeader
 
 export default function AppLayout({
   children,
@@ -14,10 +14,10 @@ export default function AppLayout({
     <SidebarProvider defaultOpen={true}>
       <AppSidebar />
       <SidebarInset className="flex flex-col">
+        <AppHeader /> {/* Add AppHeader here */}
         <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-background">
           {children}
         </main>
-        {/* FloatingVoiceInput component removed */}
       </SidebarInset>
     </SidebarProvider>
   );
